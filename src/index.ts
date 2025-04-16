5deb0852bf78bb312898217920268bfd9a79e1a6
Siddharth Suresh <siddh.suresh@gmail.com>
2025-04-16 22:19:36 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia! Version 2").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia! Version 3").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
