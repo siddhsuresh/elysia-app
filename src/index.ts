44ff7316be69b422d4c51a1ce303b30040b3c717
Siddharth Suresh <siddh.suresh@gmail.com>
2025-05-25 22:02:38 +0530

    Update dependencies and enhance logging in the application. Added @elysiajs/swagger and winston for improved API documentation and logging capabilities. Updated the Elysia app to utilize these new features, including structured logging for requests and errors.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import winston, { transports } from "winston";
 
-const app = new Elysia().get("/", () => "Hello Elysia! Version 4").listen(3000);
+const logger = winston.createLogger({
+  level: "debug",
+  format: winston.format.combine(
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console(),
+  ]
+});
 
-console.log(
+const app = new Elysia()
+  .use(swagger())
+  .get("/", ({headers}) => {
+    const requestId = headers["x-amz-cf-id"]
+    logger.info("Hi from GET", { requestId });
+    return "Hello Elysia! Version 4";
+  })
+  .get("/error", ({headers}) => {
+    const requestId = headers["x-amz-cf-id"]
+    logger.error("Error", { requestId });
+    throw new Error("Error");
+  })
+  .post("/", ({headers}) => {
+    const requestId = headers["x-amz-cf-id"]
+    logger.info("Hi from POST", { requestId });
+    return "Hello Elysia! Version 4";
+  })
+  .listen(3000);
+
+logger.debug(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
